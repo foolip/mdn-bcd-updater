41b3315bc9a89857edd8544942a66d75cef72129
Philip Jägenstedt <philip@foolip.org>
2019-02-08 10:52:34 +0100

    Add ESLint config

--- bcd.js
+++ bcd.js
@@ -1,3 +1,5 @@
+/* eslint-disable require-jsdoc */
+
 'use strict';
 
 const util = require('util');
@@ -5,22 +7,21 @@ const fs = require('fs');
 const readFile = util.promisify(fs.readFile);
 
 async function main(paths) {
-    const apis = {};
-    for (const path of paths) {
-        const json = await readFile(path, 'utf8');
-        const api = JSON.parse(json).api;
-        Object.assign(apis, api)
-    };
+  const apis = {};
+  for (const path of paths) {
+    const json = await readFile(path, 'utf8');
+    const api = JSON.parse(json).api;
+    Object.assign(apis, api);
+  };
 
-    for (const name in apis) {
-        const members = apis[name];
-        for (const memberName in members) {
-            if (memberName.startsWith('__') || memberName.endsWith('_support')) {
-                continue;
-            }
-            console.log(`${name}.${memberName}`);
-        }
+  for (const [name, members] of Object.entries(apis)) {
+    for (const memberName in members) {
+      if (memberName.startsWith('__') || memberName.endsWith('_support')) {
+        continue;
+      }
+      console.log(`${name}.${memberName}`);
     }
+  }
 }
 
 main(process.argv.slice(2));
