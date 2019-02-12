a3bb6bd9290ccc5452c40a8fc4ef064b9a341947
Philip Jägenstedt <philip@foolip.org>
2019-02-12 14:21:38 +0100

    Add skeleton of BrowserStack + Sauce remote testing

---
+++ remote.js
@@ -0,0 +1,73 @@
+/* eslint-disable require-jsdoc */
+
+'use strict';
+
+const webdriver = require('selenium-webdriver');
+
+const {argv} = require('yargs')
+    .option('service', {
+      choices: ['browserstack', 'sauce'],
+      describe: 'Where to run the tests',
+    })
+    .demandOption('service')
+    .help();
+
+async function getBrowserStackDriver(capabilities, auth) {
+  const username = auth.user;
+  const accessKey = auth.key;
+  const driver = await new webdriver.Builder()
+      .withCapabilities(Object.assign({username, accessKey}, capabilities))
+      .usingServer(`https://${username}:${accessKey}@hub-cloud.browserstack.com:443/wd/hub`)
+      .build();
+  return driver;
+}
+
+async function getSauceDriver(capabilities, auth) {
+  const username = auth.user;
+  const accessKey = auth.key;
+  const driver = await new webdriver.Builder()
+      .withCapabilities(Object.assign({username, accessKey}, capabilities))
+      .usingServer(`https://${username}:${accessKey}@ondemand.saucelabs.com:443/wd/hub`)
+      .build();
+  return driver;
+}
+
+async function main() {
+  let secrets;
+  try {
+    secrets = require('./secrets.json');
+  } catch (e) {
+    // eslint-disable-next-line max-len
+    console.error('secrets.json not found, please copy and edit secrets.sample.json');
+    process.exit(1);
+  }
+
+  let driver;
+  if (argv.service === 'browserstack') {
+    // TODO: consult https://api.browserstack.com/automate/browsers.json
+    const capabilities = {
+      browserName: 'internet explorer',
+      version: '8',
+    };
+    driver = await getBrowserStackDriver(capabilities, secrets.browserstack);
+  } else if (argv.service = 'sauce') {
+    // TODO: consult https://saucelabs.com/rest/v1/info/platforms/webdriver
+    const capabilities = {
+      browserName: 'internet explorer',
+      version: '9',
+    };
+    driver = await getSauceDriver(capabilities, secrets.sauce);
+  }
+
+  try {
+    await driver.get('https://example.com/');
+    const title = await driver.getTitle();
+    console.log(title);
+    const userAgent = await driver.executeScript('return navigator.userAgent');
+    console.log(userAgent);
+  } finally {
+    await driver.quit();
+  }
+}
+
+main();
